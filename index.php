60349553a88811f9c537d8df2338a5b4ef3435e2
huangyandong2014 <hydaxmz@163.com>
2014-12-26 00:16:12 +0800

    init project

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-    echo 'Hello World';
+    define('APP_DEBUG', TRUE);
+    
+    define('APP_PATH', './app/');
+    define('THINK_PATH', realpath('./system').'/');
+    define('RUNTIME_PATH', './runtime/');
+    
+    require_once THINK_PATH . 'ThinkPHP.php';
 ?>
